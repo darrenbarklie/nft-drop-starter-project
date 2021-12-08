a64de1921cf8707e2ac796e46c1e53ac3e254f38
dazdotdev <mail@dazdotdev>
2021-12-08 17:52:36 +0000

    Detect Phantom wallet

--- app/src/App.js
+++ app/src/App.js
@@ -1,12 +1,48 @@
-import React from 'react';
-import './App.css';
-import twitterLogo from './assets/twitter-logo.svg';
+import React, { useEffect } from "react";
+import "./App.css";
+import twitterLogo from "./assets/twitter-logo.svg";
 
 // Constants
-const TWITTER_HANDLE = '_buildspace';
+const TWITTER_HANDLE = "_buildspace";
 const TWITTER_LINK = `https://twitter.com/${TWITTER_HANDLE}`;
 
 const App = () => {
+  // Actions
+
+  const checkIfWalletIsConnected = async () => {
+    try {
+      const { solana } = window;
+
+      if (solana) {
+        if (solana.isPhantom) {
+          console.log("Phantom wallet found!");
+
+          /*
+           * The solana object gives us a function that will allow
+           * us to connect directly to the user's wallet.
+           */
+          // const response = await solana.connect({ onlyIfTrusted: true });
+          // console.log(
+          //   "Connected with Public Key: ",
+          //   response.publicKey.toString()
+          // );
+        }
+      } else {
+        alert("Solana object not found! You need a Phantom wallet.");
+      }
+    } catch (error) {
+      console.error(error);
+    }
+  };
+
+  useEffect(() => {
+    const onLoad = async () => {
+      await checkIfWalletIsConnected();
+    };
+    window.addEventListener("load", onLoad);
+    return () => window.removeEventListener("load", onLoad);
+  }, []);
+
   return (
     <div className="App">
       <div className="container">
